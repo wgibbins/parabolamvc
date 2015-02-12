3bc008895762ad9a21963496fd1ba38626ce3b72
Wilson Gibbins <PostModernLuddite@gmail.com>
2015-02-11 22:52:49 -0600

    Initial commit of ASP.NET MVC Docker Project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace MvcSample.Web
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ReturnY(int x, float a, float b, float c)
+        {
+            var y = a*(x*x) + b*x + c;
+            return Json(new object[] {x, y});         
+        }
+    }
+}
---
+++ wwwroot/Scripts/home.js
@@ -0,0 +1,43 @@
+﻿$(function() {
+    $("#go").click(clickHandler);
+    canvas = document.getElementById("parabolaCanvas");
+    ctx = canvas.getContext("2d");
+    offsetX = canvas.width / 2;
+    offsetY = canvas.height / 2;
+    minX = -offsetX;
+    maxX = offsetX;
+});
+
+var canvas;
+var ctx;
+var minX;
+var maxX;
+var offsetX;
+var offsetY;
+
+
+function clickHandler() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (var i = minX; i < maxX; i++) {
+        $.ajax({
+            url: "/Home/ReturnY",
+            data: {
+                x: i,
+                a: $("#a").val(),
+                b: $("#b").val(),
+                c: $("#c").val(),
+            },
+            success: drawPoint
+        });
+    }
+}
+
+function drawPoint(data) {
+  //  alert(data);
+    var x = offsetX + data[0];
+    var y = offsetY + data[1];
+    ctx.fillStyle = '#FF0000';
+    ctx.fillRect(x, y, 3, 3);
+}
+
+
